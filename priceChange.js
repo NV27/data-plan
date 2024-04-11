c2869f641e71636589edf3569233ccc7254a3c16
Louis <louisprotano@hotmail.com>
2024-04-11 14:05:08 +0100

    All completed (should have done more commits)

--- priceChange.js
+++ priceChange.js
@@ -4,10 +4,33 @@ var p2 = document.querySelector(".pro-price");
 var p3 = document.querySelector(".master-price");
 var b1 = document.querySelector(".annually");
 var b2 = document.querySelector(".monthly");
+var clicker = document.querySelector(".clicker");
 var m1 = 19.99;
 var m2 = 24.99;
 var m3 = 39.99;
+//b1.addEventListener("click", annualPrices);
+//b2.addEventListener("click", monthlyPrices);
 
+clicker.addEventListener("click", function(){
+    clicker.classList.toggle("cLeft");
+    console.log("clicker clicked")
+
+    if (p1.innerHTML == m1){
+        p1.innerHTML = Math.round(p1.innerHTML*=11)-0.01;
+        p2.innerHTML = Math.round(p2.innerHTML*=11)-0.01;
+        p3.innerHTML = Math.round(p3.innerHTML*=11)-0.01;
+    }
+    else{
+        p1.innerHTML = m1;
+        p2.innerHTML = m2;
+        p3.innerHTML = m3;
+    }
+    
+
+})
+
+/*
+//OLD version with individual buttons
 const prices = [m1,m2,m3];
 
 function increasePrice(inPrice){
@@ -34,6 +57,5 @@ function monthlyPrices(){
     p3.innerHTML = m3;
 
 }
+*/
 
-b1.addEventListener("click", annualPrices);
-b2.addEventListener("click", monthlyPrices)
